ad1f70e0ad9118f6a53654d5543c5e212b8e745b
Melina-Blm <melina.bouchelleghem@edu.univ-fcomte.fr>
2022-09-16 11:56:50 +0200

    finalisation formulaire

--- tdb1-vue3ts/src/components/FormulaireOffreMaison.vue
+++ tdb1-vue3ts/src/components/FormulaireOffreMaison.vue
@@ -5,18 +5,18 @@ import card from "@/components/card.vue";
 const maison = ref ({img:"/maison6.jpg"});
 </script>
 <template>
-    <div class="flex flex-row gap-10 items-center">
+    <div class="flex flex-row gap-10 items-center flex-wrap">
         <div class="p-2 w-96">
             <h2 class="text-2xl"> Résultat (Prévisualisation)</h2>
             <card v-bind="maison"/>
         </div>
-        <div class="pl-5 flex flex-col">
+        <div class=" flex flex-col bg-slate-100 p-6 rounded-lg drop-shadow-lg">
             <FormKit type="form" v-model="maison" :config="{
                 classes: {
-                    input :'p-2 rounded border-indigo-100 border ',
-                    label : 'text-gray-600 font-bold',
+                    input :'mb-5 p-1 rounded border-indigo-300 border hover:border-indigo-500 drop-shadow-lg',
+                    label : 'text-gray-600 font-semibold',
                 },
-            }" :submit-attrs="{classes: { input: 'bg-indigo-300 p-2 rounded m-4 text-center'} }">
+            }" :submit-attrs="{classes: { input: 'bg-indigo-300 p-2 rounded  text-white hover:bg-indigo-400 '} }">
              
 
    
