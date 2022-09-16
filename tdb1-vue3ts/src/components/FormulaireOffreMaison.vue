c1e4ff281d3ac22e4c8f77b37053ce3938e27c41
Melina-Blm <melina.bouchelleghem@edu.univ-fcomte.fr>
2022-09-16 11:32:53 +0200

    mise en forme du formulaire

--- tdb1-vue3ts/src/components/FormulaireOffreMaison.vue
+++ tdb1-vue3ts/src/components/FormulaireOffreMaison.vue
@@ -5,17 +5,24 @@ import card from "@/components/card.vue";
 const maison = ref ({img:"/maison6.jpg"});
 </script>
 <template>
-    <div>
+    <div class="flex flex-row gap-10 items-center">
         <div class="p-2 w-96">
             <h2 class="text-2xl"> Résultat (Prévisualisation)</h2>
             <card v-bind="maison"/>
         </div>
-        <div class="p-2">
-            <FormKit type="form" v-model="maison" >
-                <FormKit name="nom" label="nom"/>
-        
-                <FormKit name="price" label="prix" type="number"/>
-                <FormKit name="favoris" label="mettre en valeur" type="checkbox" wrapper-class="flex"/>
+        <div class="pl-5 flex flex-col">
+            <FormKit type="form" v-model="maison" :config="{
+                classes: {
+                    input :'p-2 rounded border-indigo-100 border ',
+                    label : 'text-gray-600 font-bold',
+                },
+            }" :submit-attrs="{classes: { input: 'bg-indigo-300 p-2 rounded m-4 text-center'} }">
+             
+
+   
+                <FormKit name="nom" label="Nom de l'offre "/>
+                <FormKit name="price" label="Prix de l'offre" type="number"/>
+                <FormKit name="favoris" label="Mettre en favoris" type="checkbox" wrapper-class="flex"/>
                 <FormKit name="nbbath" label="Nombre de salle de bains" type="number"/>        
                 <FormKit name="nbsize" label="Superficie m²" type="number"/>   
                 <FormKit name="txt" label="Description"/>
