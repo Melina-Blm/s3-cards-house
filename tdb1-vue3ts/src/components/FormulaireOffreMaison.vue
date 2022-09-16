63ef5671ff61099a3e012974a93da1fa483ac206
Melina-Blm <melina.bouchelleghem@edu.univ-fcomte.fr>
2022-09-16 11:09:06 +0200

    formkit name

--- tdb1-vue3ts/src/components/FormulaireOffreMaison.vue
+++ tdb1-vue3ts/src/components/FormulaireOffreMaison.vue
@@ -2,19 +2,25 @@
 import {ref} from "@vue/reactivity";
 import card from "@/components/card.vue";
 
-const maison = ref ({});
+const maison = ref ({img:"/maison6.jpg"});
 </script>
 <template>
     <div>
-        <div class="p-2">
+        <div class="p-2 w-96">
             <h2 class="text-2xl"> Résultat (Prévisualisation)</h2>
             <card v-bind="maison"/>
         </div>
         <div class="p-2">
-            <FormKit type="form" v-model="maison">
+            <FormKit type="form" v-model="maison" >
                 <FormKit name="nom" label="nom"/>
+        
                 <FormKit name="price" label="prix" type="number"/>
                 <FormKit name="favoris" label="mettre en valeur" type="checkbox" wrapper-class="flex"/>
+                <FormKit name="nbbath" label="Nombre de salle de bains" type="number"/>        
+                <FormKit name="nbsize" label="Superficie m²" type="number"/>   
+                <FormKit name="txt" label="Description"/>
+                 
+
                 </FormKit>
         </div>
        
