5cbc48c3d382082ae281c73a94f88bce29573cf0
Melina-Blm <melina.bouchelleghem@edu.univ-fcomte.fr>
2022-10-02 19:33:30 +0200

    ajout quartier et select commune

--- tdb1-vue3ts/src/components/FormulaireQuartier.vue
+++ tdb1-vue3ts/src/components/FormulaireQuartier.vue
@@ -37,11 +37,43 @@ if (error) console.log("n'a pas pu charger la table Commune :", error);
 const optionsCommune = listeCommune?.map((Commune) => ({
   value: Commune.code_commune,
   label: Commune.libelle_commune,
+  
 }));
 
+async function supprimerQuartier() {
+  const { data, error } = await supabase
+    .from("Quartier")
+    .delete()
+    .match({ code_Quartier: quartierObject.value.codeQuartier });
+  if (error) {
+    console.error(
+      "Erreur à la suppression de ",
+      quartierObject.value,
+      "erreur :",
+      error
+    );
+  } else {
+    router.push("/quartier");
+  }
+}
     </script>
     <template>
-      <FormKit type="form">
-        <!-- TODO -->
+       <FormKit type="form" submit-label="Submit" @submit="upsertQuartier" :config="{
+                classes: {
+                    input :'mb-5 p-1 rounded border-indigo-300 border hover:border-indigo-500 drop-shadow-lg',
+                    label : 'text-gray-600 font-semibold',
+                },
+            }" :submit-attrs="{classes: { input: 'bg-indigo-300 p-2 rounded  text-white hover:bg-indigo-400 '} }">
+            <FormKit name="libelle_quartier" label="Ajout d'un quartier"></FormKit>
+        <FormKit
+        type="select"
+        name="code_commune"
+        label="Commune"
+        :options="optionsCommune"
+      />
+  
       </FormKit>
+
+   
+
     </template>
